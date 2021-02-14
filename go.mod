e31b73fd1897f228103affb8d710b9626b9f488c
wanglongxiang <bruce@mark.com>
2021-02-14 21:52:53 +0800

    修复系统生成不能启动的问题

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module helloworld
 go 1.15
 
 require (
+	github.com/asim/go-micro/v3 v3.5.0
+	github.com/golang/protobuf v1.4.3
 	github.com/micro/micro/v3 v3.0.0
+	google.golang.org/protobuf v1.25.0
 )
 
 // This can be removed once etcd becomes go gettable, version 3.4 and 3.5 is not,
--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"github.com/asim/go-micro/v3"
 	"helloworld/handler"
 	pb "helloworld/proto"
-
-	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
 )
 
 func main() {
 	// Create service
-	srv := service.New(
-		service.Name("helloworld"),
-		service.Version("latest"),
+	srv := micro.NewService(
+		micro.Name("helloworld"),
+		micro.Version("latest"),
 	)
 
 	// Register handler
